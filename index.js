fdb9570ea88932c5be1b3c155b078d1598bae7a9
Deren Y <67863795+Deren15@users.noreply.github.com>
2024-03-23 14:10:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ app.post('/checkprime', (req, res) => {
     }
 });
 
+app.use(express.static('public'));
+app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
